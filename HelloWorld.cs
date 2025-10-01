e3f437f6bfdacb6d6efe862365138d4ad90e4d60
OnkarThombre <163149992+OnkarThombre@users.noreply.github.com>
2025-10-01 12:58:02 +0530

    HelloWorld.cs

---
+++ HelloWorld.cs
@@ -0,0 +1,13 @@
+// simple C# code just like Hello World 
+
+using System;
+namespace Assignments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Onkar Thombre");
+        }
+    }
+}
